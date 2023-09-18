7a2133974825d14f0178aabfa7bc8602c1b507ec
Saymon <mdsymon703@gmail.com>
2023-09-18 14:12:00 +0600

    dev-saymon

--- module1/assignment/classNote.php
+++ module1/assignment/classNote.php
@@ -228,7 +228,7 @@
 // }
 // echo symon(12,12)+10;
 
-//null retun type and smart way
+//null retun type and smart way================>
 
 // function symon($s,$k):?int{
 //     $sum= ($s+$k)/2;
@@ -236,76 +236,6 @@
 // }
 // echo symon(12,12)+20;
 
-// $name="hello my name is saimon";
-//  $namev=strlen($name);
-//  echo $namev;
-
-// function add(){
-//     $x=10;
-//     $y=20;
-
-//     echo $x + $y." <br/> ";
-// }
-// add();
-// add();
-// add();
-
-// function add( $x,$y,$z){
-//     echo $x+$y+$z;
-
-// }
-// add(12,23,4);
-
-// function addfuntion($x , $y,$z=10){
-//     echo $x+$y+$x;
-// }
-
-// addfuntion(10,20,);
-
-// function addfun(string $name , int $number){
-// echo "hello my name is $name and my age $number";
-// }
-// addfun("symon",20);
-
-// declare(strict_types=1);
-
-// function addfun(string $name , int $number){
-//     echo "hello my name is $name and my age $number";
-//     }
-//     addfun("symon","20");
-
-
-// function FunctionName(?string $name){
-//     echo $name;
-// }
-
-// FunctionName("symon");
-// FunctionName(null);
-
-// function FunctionName(...$sum) {
-//     echo array_sum($sum);
-// }
-// FunctionName(1,2,3,4,);
-
-// function FunctionName(...$sum) {
-// //     echo $sum[1];
-// // }
-// // FunctionName(1,2,3,4,);
-
-// $symon=fn($x,$y)=>$x+$y;
-// echo $symon(10,20);
-
-// function FunctionName($num,$num2) : float|bool|string|int{
-//     $sum=($num+$num2)/2;
-//     return $sum;
-// }
-// echo FunctionName(10,20)+20;
-
-// function FunctionName($num,$num2) :void{
-//     $sum=($num+$num2)/2;
-//     return $sum;
-// }
-// echo FunctionName(10,20)+20;
 
 
 //today class date 17/09/2023==================================>>>

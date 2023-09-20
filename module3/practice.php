d1383bf7a6ff46cf27a93198498bb2f5c93eeecc
Saymon <mdsymon703@gmail.com>
2023-09-20 15:43:54 +0600

    module3

--- module3/practice.php
+++ module3/practice.php
@@ -1,2 +1,54 @@
 <?php
-echo "hello would";
+//INDEX ARRAY==============>
+
+// $arry1=array("mitu","tumpa","symon","rumpa");
+// $arry2=["mitu","tumpa","symon","rumpa"];
+// echo $arry1[0].PHP_EOL;
+// echo $arry2[2];
+
+//loop array====>
+
+// $arry1=array("mitu","tumpa","symon","rumpa");
+// $arry2=["mitu","tumpa","symon","rumpa"];
+
+// for($i=0;$i<count($arry1);$i++){
+//     echo $arry1[$i] ."<br/>";
+// }
+
+//foreach loop dia array count kora========> 
+
+// $arry1=array("mitu","tumpa","symon","rumpa");
+// $arry2=["mitu","tumpa","symon","rumpa"];
+
+// foreach($arry1 as $value){
+//     echo $value."<br/>";
+// }
+
+//Associative arrays====>
+
+// $man= array("fristName"=>"symon","lastName"=>"hossen");
+// $man1= ["fristName"=> "samir", "lastName"=>"ahmed"];
+
+// //accessing array====>
+
+// echo $man["fristName"];
+
+// foreach($man as $key=> $value);
+
+//multidimensional array======>
+
+$array= array(
+    array(1,2,3),
+    array(4,5,6),
+    array(7,8,9)
+);
+echo $array[2][3];
+
+
+
+
+
+
+
+
+

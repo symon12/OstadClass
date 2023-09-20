b7a177f7a3c8d1af85563b4af6578f7dd3fcc2f1
Saymon <mdsymon703@gmail.com>
2023-09-20 20:24:24 +0600

    module3

--- module3/practice.php
+++ module3/practice.php
@@ -37,12 +37,55 @@
 
 //multidimensional array======>
 
-$array= array(
-    array(1,2,3),
-    array(4,5,6),
-    array(7,8,9)
-);
-echo $array[2][3];
+//  $array=[
+//     ['a','b','c'],
+//     ['d','e','f'],
+//     ['g','h','i'],
+// ];
+// print_r($array);
+// // echo $array[1][2];
+
+// $array=[
+//     ["frist_name"=>"shrower" , "frist_name"=>"hossen","last_name"=>"symon"],
+//     ["frist_name"=>"ahad" , "frist_name"=>"jaman","last_name"=>"neloy"],
+//     ["frist_name"=>"imam" , "frist_name"=>"uddin","last_name"=>"nahid"],
+// ];
+// // echo $array[2]["frist_name"]."<br/>".$array[1]["last_name"];
+
+// // echo $array[2]["frist_name"];
+// // echo $array[2]["last_name"];
+
+// foreach($array as $child){
+// foreach($child as $key=>$valu){
+//     echo "<br/>".$valu ; 
+// }
+
+// }
+
+// $users = array(
+//     "john" => array(
+//         "age" => 25,
+//         "email" => "john@example.com"
+//     ),
+//     "jane" => array(
+//         "age" => 30,
+//         "email" => "jane@example.com"
+//     )
+// );
+
+// echo $users["john"]["email"];
+
+$user= array(
+    "john"=> array(
+        "age"=>25,
+        "email"=>"mdsymon703@gmail.com"
+    ),
+    "symon1"=>array(
+        "age"=> 4,
+        "email"=>"mdsymon703@gmail.com"
+    )
+    );
+    echo $user["john"]["age"];
 
 
 

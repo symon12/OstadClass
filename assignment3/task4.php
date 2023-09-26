1b6cbe7e320063447c1358b644db0e72be04c7db
Saymon <mdsymon703@gmail.com>
2023-09-26 17:16:42 +0600

    assignment3

--- assignment3/task4.php
+++ assignment3/task4.php
@@ -1,32 +1,32 @@
 <?php
 
 echo "Task 4: Multidimensional Array \n";
-$studentGrade=array(
-    "student1"=>array(
-        "Math"=>85,
-        "English"=>92,
-        "science"=>70
+
+$studentGread = array(
+    array(
+            "Math" => 82,
+            "English" => 56,
+            "Science" => 96
+        ),
+    array(
+        "Math" => 75,
+        "English" => 33,
+        "Science" => 74
     ),
-    "student2"=>array(
-        "Math"=>91,
-        "English"=>87,
-        "science"=>95
-    ),
-    "student3"=>array(
-        "Math"=>67,
-        "English"=>66,
-        "science"=>77
-    ),
-);
+    array(
+        "Math" => 85,
+        "English" => 65,
+        "Science" => 67
+    ));
 
 function calculatorAveragesNumber($grade){
-    foreach($grade as  $studentName =>$studentNumber){
+    foreach($grade as  $studentNumber){
         $AdditionNum=array_sum($studentNumber);
         $countNum=count($studentNumber);
         $AveragesNum= $AdditionNum / $countNum;
-        echo "Average Grade for $studentName : $AveragesNum \n";
+        echo "Average Grade for : $AveragesNum \n";
     }
 }
-calculatorAveragesNumber($studentGrade);
+calculatorAveragesNumber($studentGread);
 
 
